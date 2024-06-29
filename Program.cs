7c0cf25ae48cc4e38adc36d88c756059c524e22b
Kallory <kory232323@gmail.com>
2024-06-28 22:27:57 -0500

    Finishing task

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 Console.WriteLine("This program takes the squares of the first 100 natural numbers and subtracts it from square of the sum of the first 100 natural numbers.");
-Console.WriteLine(squareOfSums());
+Console.WriteLine("sum of squares = " + sumOfSquares(100));
+Console.WriteLine("Square of sums = " + squareOfSums(100));
+Console.WriteLine("Difference between the sum of the squares of the first one hundred natural numbers and the square of the sum = " + diffBetweenSumOfSquaresAndSquareOfSums(squareOfSums(100), sumOfSquares(100)));
     
-double sumOfSquares()
+double sumOfSquares(int num)
 {
     double sum = 0;
-    for (double i = 1; i <= 100; i++)
+    for (double i = 1; i <= num; i++)
     {
         sum += Math.Pow(i, 2);
     }
@@ -13,13 +15,18 @@ double sumOfSquares()
     return sum;
 }
 
-int squareOfSums()
+double squareOfSums(int num)
 {
-    int square = 0;
-    for (int i = 1; i <= 100; i++)
+    double square = 0;
+    for (int i = 1; i <= num; i++)
     {
-
+        square += i;
     }
 
-    return square;
+    return Math.Pow(square, 2);
+}
+
+double diffBetweenSumOfSquaresAndSquareOfSums(double square, double sum)
+{
+    return square - sum;
 }
